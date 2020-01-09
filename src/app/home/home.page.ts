4bbb61c5b2ed3543183abbb420a39f4d0ff2f6ee
Junior Rojas <developer@bnf.com.co>
2020-01-08 20:35:19 -0500

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,88 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+declare var google;
 
+interface Marker {
+  position: {
+    lat: number,
+    lng: number,
+  };
+  title: string;
+}
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  map = null
+  markers: Marker[] = [
+    {
+      position: {
+        lat: 4.658383846282959,
+        lng: -74.09394073486328,
+      },
+      title: 'Parque Simón Bolivar'
+    },
+    {
+      position: {
+        lat: 4.667945861816406,
+        lng: -74.09964752197266,
+      },
+      title: 'Jardín Botánico'
+    },
+    {
+      position: {
+        lat: 4.676802158355713,
+        lng: -74.04825592041016,
+      },
+      title: 'Parque la 93'
+    },
+    {
+      position: {
+        lat: 4.6554284,
+        lng: -74.1094989,
+      },
+      title: 'Maloka'
+    },
+  ];
+  constructor() {
+  }
 
-  constructor() {}
+  ngOnInit() {
+    this.loadMap();
+  }
+
+
+  loadMap() {
+    // create a new map by passing HTMLElement
+    const mapEle: HTMLElement = document.getElementById('map');
+    // create LatLng object
+    const myLatLng = { lat: 4.658383846282959, lng: -74.09394073486328 };
+    // create map
+    this.map = new google.maps.Map(mapEle, {
+      center: myLatLng,
+      zoom: 12
+    });
+
+    google.maps.event.addListenerOnce(this.map, 'idle', () => {
+      this.renderMarkers();
+      mapEle.classList.add('show-map');
+    });
+  }
+
+  addMarker(marker: Marker) {
+    return new google.maps.Marker({
+      position: marker.position,
+      map: this.map,
+      title: marker.title
+    });
+  }
+
+  renderMarkers() {
+    this.markers.forEach(marker => {
+      this.addMarker(marker);
+    });
+  }
 
 }
+
